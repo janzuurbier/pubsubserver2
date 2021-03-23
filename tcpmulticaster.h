f0f8dd7ef76ad3e650721ff67fd239885b096010
Jan Zuurbier <jan.zuurbier@hu.nl>
2021-03-23 22:24:14 +0100

    thread safe gemaakt met een mutex

--- tcpmulticaster.h
+++ tcpmulticaster.h
@@ -5,6 +5,7 @@
 
 #include <map>
 #include <set>
+#include <mutex>
 #include "PracticalSocket.h"
 
 using namespace std;
@@ -16,6 +17,7 @@ class tcp_multicaster
 	
 private:
 	map<string, set<TCPSocket*> > subscriptions;
+	std::mutex   mtx;
 	
 	
 public:
@@ -23,6 +25,7 @@ public:
 
 	
 	void subscribe(TCPSocket* sock, string topic){
+		std::lock_guard<std::mutex> lock(mutex);
 		if (subscriptions.count(topic) == 0) {
 			set<TCPSocket*> myset;
 			subscriptions[topic] = myset;
@@ -31,21 +34,18 @@ public:
 	}
 	
 	void unsubscribe(TCPSocket* sock, string topic){
+		std::lock_guard<std::mutex> lock(mutex);
 		subscriptions[topic].erase(sock);
 	}
 	
 	void send(char* buffer, int len, string topic){
-		
-		
+		std::lock_guard<std::mutex> lock(mutex);	
 		for (std::set<TCPSocket*>::iterator it = subscriptions[topic].begin(); it != subscriptions[topic].end(); ++it)
 			try { 
 					(*it)->send(buffer, len); 
 			} 
 			catch (SocketException& e) { cout << "oeps " << e.what() << endl; /*it = subscriptions[topic].erase(it);*/ }
-
 	}
-
-
 
 };
 
